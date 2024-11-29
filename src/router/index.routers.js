fc2901b841571581a696be1a2b66bba9f12187e7
Yeison Manuel Mestra Mestra <ymmestram@ufpso.edu.co>
2024-11-29 15:49:10 -0500

    inicio

--- src/router/index.routers.js
+++ src/router/index.routers.js
@@ -7,6 +7,19 @@ routerIndex.use("/api",routerPersonaje)
 routerIndex.use("/api",routerPlaneta)
 
 routerIndex.get("/", (req, res)=>{
-    return res.json({data: "Hola mundo"});
+    return res.json({
+        data: {
+            time: Date.now(),
+            status: 200,
+            message: "Welcome to the API of Dragon Ball",
+            by : "Yeison Mestra",
+            routes :{
+                personajes : "/api/personaje",
+                personajesUnico : "/api/personaje/unico",
+                planetas : "/api/planeta",
+                planetasUnico : "/api/planeta/:id_planeta"
+            }
+        }
+    });
 })
 export default routerIndex;
